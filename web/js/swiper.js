8b73ab99a479f5e8ecc2c10fe2050211a613a3e9
monstredd <cestlaviedd@outlook.com>
2022-07-11 12:09:52 +0800

    webfront

---
+++ web/js/swiper.js
@@ -0,0 +1,26 @@
+// swiper.js
+
+var swiper = new Swiper('.clients-slider', {
+    slidesPerView: 3,
+    spaceBetween: 18,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }
+  });
